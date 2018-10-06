7843f49e746beff82ad329c1c1acd5e90301b7fa
Chanil Park <ncksdlf92@gmail.com>
2018-10-07 05:41:42 +1300

    Update utf8Encoder.js

--- utf8Encoder.js
+++ utf8Encoder.js
@@ -1,7 +1,7 @@
 // Encode everything to utf-8
 // Modified source code from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
 function fixedEncodeURIComponent(str) {
-  return encodeURIComponent(str).replace(/[-|.|!|~|*|'|(|)|\w]/g, function(c) {
+  return encodeURIComponent(str).replace(/[\-|\.|\!|\~|\*|\'|\(|\)|\w]/g, function(c) {
     return '%' + c.charCodeAt(0).toString(16);
   });
 }
